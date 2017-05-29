c31fc5daa05c12c5b8b90e49f68b134c42180ec4
James Gill <james@roboticmonsters.com>
2017-05-29 12:15:29 +1000

    update PropType usage to support React 15.5.0 (remove warnings) update dependencies to include `prop-types` Related Issues: [#53](https://github.com/developerdizzle/react-virtual-list/issues/53)

--- src/VirtualList.js
+++ src/VirtualList.js
@@ -1,5 +1,6 @@
-import React, { PureComponent, PropTypes } from 'react';
+import React, { PureComponent } from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 
 import getVisibleItemBounds from './utils/getVisibleItemBounds';
 import throttleWithRAF from './utils/throttleWithRAF';
